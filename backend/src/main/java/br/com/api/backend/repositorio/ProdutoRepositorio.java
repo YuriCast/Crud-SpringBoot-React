dd5cae889aef82c9c42682082e1ac46cb69184d0
Yuri Castilho <yuricastilho2005@gmail.com>
2023-09-08 00:03:39 -0300

    feat: Configure repository

--- backend/src/main/java/br/com/api/backend/repositorio/ProdutoRepositorio.java
+++ backend/src/main/java/br/com/api/backend/repositorio/ProdutoRepositorio.java
@@ -1,5 +1,11 @@
 package br.com.api.backend.repositorio;
 
-public interface ProdutoRepositorio {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.api.backend.modelo.ProdutoModelo;
+
+@Repository
+public interface ProdutoRepositorio extends CrudRepository<ProdutoModelo, Long>{
     
 }
