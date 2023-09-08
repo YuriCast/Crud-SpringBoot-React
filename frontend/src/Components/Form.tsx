383cd429fabb94fa305bf428827f5d92741f8fb7
Yuri Castilho <yuricastilho2005@gmail.com>
2023-09-08 12:03:22 -0300

    style: Table structure

---
+++ frontend/src/Components/Form.tsx
@@ -0,0 +1,13 @@
+export default function Form() {
+  return (
+    <form>
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Brand" />
+
+      <input type="button" value="Register" />
+      <input type="button" value="Change" />
+      <input type="button" value="Delete" />
+      <input type="button" value="Calcel" />
+    </form>
+  )
+}
---
+++ frontend/src/Components/Table.tsx
@@ -0,0 +1,22 @@
+export default function Table() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Brand</th>
+          <th>Select</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
