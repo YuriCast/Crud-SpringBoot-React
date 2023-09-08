215cfc03d3b2eaa65852a81a7582e4ff149f37cb
Yuri Castilho <yuricastilho2005@gmail.com>
2023-09-08 00:25:11 -0300

    feat: Route to list products

--- backend/src/main/java/br/com/api/backend/control/ProductControl.java
+++ backend/src/main/java/br/com/api/backend/control/ProductControl.java
@@ -1,14 +1,25 @@
 package br.com.api.backend.control;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import br.com.api.backend.model.ProductModel;
+import br.com.api.backend.service.ProductService;
 
+@RestController
 public class ProductControl {
 
+    @Autowired
+    private ProductService ps;
+
+    @GetMapping("/list")
+    public Iterable<ProductModel> list(){
+        return ps.list();
+    }
+
     @GetMapping("/")
-    public String rota(){
+    public String route(){
         return "API de produtos funcionando";
     }
 
