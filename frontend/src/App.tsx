bde04a4eb7a194646417c4dcd18c3d407b037c16
Yuri Castilho <yuricastilho2005@gmail.com>
2023-09-10 17:02:58 -0300

    feat: Get the products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Form from './Components/Form/Form'
 import Table from './Components/Table/Table'
 
 function App() {
-  const [btnCadastrar, setBtnCadastrar] = useState(true);
-  
 
+  // UseState
+  const [btnRegister, setBtnRegister] = useState(true);
+  const [products, setProducts] = useState([])
+  
+  // UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/list")
+    .then(response => response.json())
+    .then(converted_response => setProducts(converted_response));
+  }, []);
+
+  // Return
   return (
     <>
-      <Form btn={btnCadastrar}/>
+    <p>{JSON.stringify(products)}</p>
+      <Form btn={btnRegister}/>
       <Table />
     </>
   )
