744c210ed2b3c279ff2444a1a1be86a32928e682
Yuri Castilho <yuricastilho2005@gmail.com>
2023-09-10 18:03:11 -0300

    feat: Create product object

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,13 @@ import Table from './Components/Table/Table'
 
 function App() {
 
+  // Product Object
+  const product = {
+    id : 0,
+    name : '',
+    brand : '' 
+  }
+
   // UseState
   const [btnRegister, setBtnRegister] = useState(true);
   const [products, setProducts] = useState([])
